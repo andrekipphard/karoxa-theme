ffb6cda69f03cd86b8ed0ca29c84bf40a2673aaf
andrekipphard <andrekipphard@gmail.com>
2023-05-22 21:30:49 +0200

    Test

--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -12,7 +12,6 @@
 	<header class="page-header">
 		<h1 class="page-title"><?php esc_html_e( 'Nothing Found', 'rooxy' ); ?></h1>
 	</header><!-- .page-header -->
-
 	<div class="page-content">
 		<?php
 		if ( is_home() && current_user_can( 'publish_posts' ) ) :
