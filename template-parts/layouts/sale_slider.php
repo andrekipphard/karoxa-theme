2a7c40d79c2690bf7f88399d12c1ecf10272a003
andrekipphard <andrekipphard@gmail.com>
2023-07-04 12:48:34 +0200

    made products 2 column in sale slider, changed h2 font size in footer, changed badges size on mobile

--- template-parts/layouts/sale_slider.php
+++ template-parts/layouts/sale_slider.php
@@ -55,7 +55,7 @@
                 </div>
             </div>
             <div class="row pb-0 pb-lg-3">
-                <div class="col">
+                <div class="col px-0">
                     <!-- Desktop Slider -->
                     <div id="carouselExample" class="carousel slide carousel-dark carousel-desktop">
                         <div class="carousel-inner carousel-flex">
@@ -173,22 +173,72 @@
                             </button>
                         <?php endif;?>
                     </div>
-                    <!-- Mobile Slider -->
                     <?php 
                         $index = 0;
                     ?>
+                    <!-- Mobile Slider 2 Columns -->
                     <div id="carouselMobile" class="carousel slide carousel-dark carousel-mobile" data-ride="carousel" data-touch="true">
                         <div class="carousel-inner carousel-flex">
-                            <div class="carousel-item active p-0 p-lg-3 col-12">
+                            <div class="carousel-item active col-12">
+
+
                                 <?php 
-                                    while($index<1 && !empty($products[$index])):
+                                    while($index<2 && !empty($products[$index])):
                                         $product_title = $products[$index]->get_title();
                                         $product_permalink = $products[$index]->get_permalink();
                                         $product_cats = $products[$index]->get_categories();
                                         $product_price = $products[$index]->get_price_html();
                                         $product_subcats = get_product_subcategories($product_id);    
                                 ?>
-                                        <div class="col-12 col-lg-3 carousel-col rounded shadow p-4">
+
+                                        <div class="col-6 col-lg-3 carousel-col rounded shadow p-4">
+                                            <a href="<?=$product_permalink?>"><img class="img-fluid mb-2" src="<?php echo wp_get_attachment_url( $products[$index]->get_image_id(), 'large' ); ?>" /></a>
+                                            <?php 
+                                                if ( ! $products[$index]->is_in_stock() ) {
+                                                    echo '<div class="outofstock-badge pt-1 pb-1 ps-3 pe-3 lemonmilk">' . esc_html__( 'AUSVERKAUFT', 'woocommerce' ) . '</div>';
+                                                }
+
+                                                else if( $products[$index]->is_on_sale() ) {
+                                                    echo '<div class="onsale-badge pt-1 pb-1 ps-3 pe-3 lemonmilk">' . esc_html__( 'SALE', 'woocommerce' ) . '</div>';
+                                                }
+
+                                                
+                                            ?>
+                                            <div class="p carousel-produkt-subkategorie mb-2">
+                                                <?php $category_count = 1;?>
+                                                <?php foreach ($product_subcats as $subcategory):?>
+                                                        <a href="<?=$subcategory['url'];?>"><?= $subcategory['name'];?></a><?php if($category_count<count($product_subcats)):?>,<?php endif;?>
+                                                        <?php $category_count++;?>
+                                                        <?php endforeach;
+                                                ?>
+                                            </div>
+                                            
+                                            <div class="carousel-produkt-titel mb-2">
+                                                <?= $product_title;?>
+                                            </div>
+                                            <div class="p carousel-produkt-preis">
+                                                <?= $product_price;?>
+
+                                            </div>
+                                        </div>
+                                <?php
+                                        $index++;
+                                    endwhile;
+                                ?>
+                            </div>
+                            <?php $index_items++;?>
+                            <?php while ($index < $array):?>
+                                <?php $index_loop_item = 0;?>
+                                <div class="carousel-item col-12">
+                                    <?php while($index_loop_item < 2):
+                                        $product_title = $products[$index]->get_title();
+                                        $product_permalink = $products[$index]->get_permalink();
+                                        $product_cats = $products[$index]->get_categories();
+                                        $product_price = $products[$index]->get_price_html();
+                                        $product_currency = get_woocommerce_currency();
+                                        $product_subcats = get_product_subcategories($product_id);  
+                                    ?>
+                                        <div class="col-6 col-lg-3 carousel-col rounded shadow p-4">
                                             <a href="<?=$product_permalink?>"><img class="img-fluid mb-2" src="<?php echo wp_get_attachment_url( $products[$index]->get_image_id(), 'large' ); ?>" /></a>
                                             <?php 
                                                 if ( ! $products[$index]->is_in_stock() ) {
@@ -210,7 +260,7 @@
                                             </div>
                                             <div class="p carousel-produkt-kategorie mb-2 d-flex flex-wrap">
                                                 <!--<a href="/produkt-kategorie/<?=$kategorie_slug?>/" class="pt-1 pb-1 pe-3 ps-3 rounded"><?= $kategorie?></a>-->
-                                                <?php print_product_categories($products[$index]->get_id());?>
+                                                <!--<?php print_product_categories($products[$index]->get_id());?>-->
                                             </div>
                                             <div class="carousel-produkt-titel mb-2">
                                                 <?= $product_title;?>
@@ -220,59 +270,15 @@
 
                                             </div>
                                         </div>
-                                <?php
-                                        $index++;
-                                    endwhile;
-                                ?>
-                            </div>
-                            <?php while ($index < $array):?>
-                                <div class="carousel-item p-0 p-lg-3 col-12">
-                                    <?php
-                                        $product_title = $products[$index]->get_title();
-                                        $product_permalink = $products[$index]->get_permalink();
-                                        $product_cats = $products[$index]->get_categories();
-                                        $product_price = $products[$index]->get_price_html();
-                                        $product_currency = get_woocommerce_currency();
-                                        $product_subcats = get_product_subcategories($product_id);  
-                                    ?>
-                                    <div class="col-12 col-lg-3 carousel-col rounded shadow p-4">
-                                        <a href="<?=$product_permalink?>"><img class="img-fluid mb-2" src="<?php echo wp_get_attachment_url( $products[$index]->get_image_id(), 'large' ); ?>" /></a>
-                                        <?php 
-                                            if ( ! $products[$index]->is_in_stock() ) {
-                                                echo '<div class="outofstock-badge pt-1 pb-1 ps-3 pe-3 lemonmilk">' . esc_html__( 'AUSVERKAUFT', 'woocommerce' ) . '</div>';
-                                            }
-                                            else if( $products[$index]->is_on_sale() ) {
-                                                echo '<div class="onsale-badge pt-1 pb-1 ps-3 pe-3 lemonmilk">' . esc_html__( 'SALE', 'woocommerce' ) . '</div>';
-                                            }
-
-                                            
+                                        <?php $index++;?>
+                                        <?php $index_loop_item++;?>
+                                        <?php if($index >= $array):
+                                            break;
                                         ?>
-                                        <div class="p carousel-produkt-subkategorie mb-2">
-                                            <?php $category_count = 1;?>
-                                            <?php foreach ($product_subcats as $subcategory):?>
-                                                    <a href="<?=$subcategory['url'];?>"><?= $subcategory['name'];?></a><?php if($category_count<count($product_subcats)):?>,<?php endif;?>
-                                                    <?php $category_count++;?>
-                                                    <?php endforeach;
-                                            ?>
-                                        </div>
-                                        <div class="p carousel-produkt-kategorie mb-2 d-flex flex-wrap">
-                                            <!--<a href="/produkt-kategorie/<?=$kategorie_slug?>/" class="pt-1 pb-1 pe-3 ps-3 rounded"><?= $kategorie?></a>-->
-                                            <?php print_product_categories($products[$index]->get_id());?>
-                                        </div>
-                                        <div class="carousel-produkt-titel mb-2">
-                                            <?= $product_title;?>
-                                        </div>
-                                        <div class="p carousel-produkt-preis">
-                                            <?= $product_price;?>
-
-                                        </div>
-                                    </div>
-                                    <?php $index++;?>
-                                    <?php if($index >= $array):
-                                        break;
-                                    ?>
-                                    <?php endif;?>
+                                        <?php endif;?>
+                                    <?php endwhile;?>
                                 </div>
+                                <?php $index_items++;?>
                             <?php endwhile;?>
                         </div>
                         <?php if($index_items>1):?>
@@ -284,7 +290,6 @@
                                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
                                 <span class="visually-hidden">Next</span>
                             </button>
-                            
                         <?php endif;?>
                     </div>
                 </div>
