d84685f35f67594b2b0f3114fdf48497ce952f76
andrekipphard <andrekipphard@gmail.com>
2023-07-04 11:57:27 +0200

    made product grid on mobile 2 columns

--- template-parts/layouts/products_of_category.php
+++ template-parts/layouts/products_of_category.php
@@ -22,9 +22,17 @@
                         </div>
                     </div>
                 </div>
-                <div class="row pb-3 pb-lg-5 ps-0 ps-lg-3 pe-0 pe-lg-3 products-of-category">
-                    <div class="col">
+                <div class="row pb-3 pb-lg-0 ps-0 ps-lg-3 pe-0 pe-lg-3 products-of-category">
+                    <div class="col mobile-hide">
                         <?php echo do_shortcode('[product_category category="' . $category->slug . '" columns="4" limit="8"]'); ?>
+                    </div>
+                    <div class="col desktop-hide">
+                        <?php echo do_shortcode('[product_category category="' . $category->slug . '" columns="4" limit="4"]'); ?>
+                    </div>
+                </div>
+                <div class="row pb-3 pb-lg-5 mb-lg-5 text-center">
+                    <div class="col">
+                        <a href="<?= $category_link ?>"><btn class="btn btn-primary">Mehr anzeigen</btn></a>
                     </div>
                 </div>
                 <?php
